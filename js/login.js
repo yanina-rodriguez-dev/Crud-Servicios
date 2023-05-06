bc846c4e31d8b3e1faaa739143cfee325eaaa449
Nicolas Herrera <herreranicoo17@gmail.com>
2023-05-06 16:38:24 -0300

    Cree la funcion limpiarFormulario y reordene el codigo de validar datos, e iniciar sesion

--- js/login.js
+++ js/login.js
@@ -1,40 +1,39 @@
 const usuarioAdmin = "administrador",
-  contraseñaAdmin = "administrador",
+  passwordAdmin = "administrador",
   inputUsuario = document.getElementById("inputUsuario"),
-  inputContraseña = document.getElementById("inputContraseña"),
+  inputPassword = document.getElementById("inputPassword"),
   btnIniciarSesion = document.getElementById("btnIniciarSesion"),
   formLogin = document.getElementById("formLogin");
 
 formLogin.addEventListener("submit", (e) => {
   e.preventDefault();
   validarDatos();
-  formLogin.reset();
-  inputContraseña.blur();
 });
 
 function validarDatos() {
-  let usuarioIngresado = inputUsuario.value.trim();
-  let contraseñaIngresada = inputContraseña.value.trim();
-  let esTexto = /^[a-zA-Z]+$/;
+  let usuarioIngresado = inputUsuario.value.trim(),
+    passwordIngresada = inputPassword.value.trim();
+  if (usuarioIngresado !== "" && passwordIngresada !== "") {
+    iniciarSesion(usuarioIngresado, passwordIngresada);
+  }
+  if (usuarioIngresado === "" || passwordIngresada === "") {
+    console.log("Los campos son obligatorios y no pueden estar vacios.");
+  }
+  limpiarFormulario();
+  inputPassword.blur();
+}
+
+function iniciarSesion(usuarioIngresado, passwordIngresada) {
   if (
-    esTexto.test(usuarioIngresado) &&
-    esTexto.test(contraseñaIngresada) &&
-    usuarioIngresado !== "" &&
-    contraseñaIngresada !== ""
+    usuarioIngresado === usuarioAdmin &&
+    passwordIngresada === passwordAdmin
   ) {
-    iniciarSesion();
+    console.log("Administrador logueado.");
   } else {
-    console.log("Los datos ingresados no son válidos.");
+    console.log("Usuario o contraseña incorrectos.");
   }
 }
 
-function iniciarSesion() {
-      if (
-        usuarioIngresado === usuarioAdmin &&
-        contraseñaIngresada === contraseñaAdmin
-      ) {
-        console.log("Administrador logueado.");
-      } else {
-        console.log("Usuario o contraseña incorrectos.");
-      }
+function limpiarFormulario() {
+  formLogin.reset();
 }
