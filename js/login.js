b08384bae374b064477f252661ebd1eb94830fc3
Nicolas Herrera <herreranicoo17@gmail.com>
2023-05-06 14:11:18 -0300

    Agregue id para formulario login en login.html, declare la variable del elemento, añadí la detección del evento submit y agregue prevent default.

--- js/login.js
+++ js/login.js
@@ -2,4 +2,9 @@ const usuarioAdmin = "administrador",
   contraseñaAdmin = "administrador",
   inputUsuario = document.getElementById("inputUsuario"),
   inputContraseña = document.getElementById("inputContraseña"),
-  btnIniciarSesion = document.getElementById("btnIniciarSesion");
+  btnIniciarSesion = document.getElementById("btnIniciarSesion"),
+  formLogin = document.getElementById("formLogin");
+
+  formLogin.addEventListener("submit", (e) => {
+    e.preventDefault();
+  })
