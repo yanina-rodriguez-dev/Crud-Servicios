c87a4e8a7940ad6604117eaaf875028e7cdf2e7f
Nicolas Herrera <herreranicoo17@gmail.com>
2023-05-06 14:41:19 -0300

    Agregue funcion validar datos, la misma verifica que sea un texto y no este vacío

--- js/login.js
+++ js/login.js
@@ -7,4 +7,18 @@ const usuarioAdmin = "administrador",
 
   formLogin.addEventListener("submit", (e) => {
     e.preventDefault();
+    validarDatos()
+    formLogin.reset()
+    inputContraseña.blur()
   })
+
+  function validarDatos() {
+    let usuarioIngresado = inputUsuario.value.trim()
+    let contraseñaIngresada = inputContraseña.value.trim()
+    let esTexto = /^[a-zA-Z]+$/;
+    if (esTexto.test(usuarioIngresado) && esTexto.test(contraseñaIngresada) && usuarioIngresado !== "" && contraseñaIngresada !== "") {
+      console.log("Administrador logueado.");
+    } else {
+      console.log("Usuario sin acceso.");
+    }
+  }
