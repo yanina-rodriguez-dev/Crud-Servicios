019d0f15a6f170be20c3fc961e6a46b88a0bc508
Nicolas Herrera <herreranicoo17@gmail.com>
2023-05-06 15:33:06 -0300

    La funcion validar datos ahora detecta si el usuario y la contraseña ingresada son iguales a las credenciales de administrador, si no son iguales, detecta si el usuario o la contraseña estan mal ingresados

--- js/login.js
+++ js/login.js
@@ -5,20 +5,44 @@ const usuarioAdmin = "administrador",
   btnIniciarSesion = document.getElementById("btnIniciarSesion"),
   formLogin = document.getElementById("formLogin");
 
-  formLogin.addEventListener("submit", (e) => {
-    e.preventDefault();
-    validarDatos()
-    formLogin.reset()
-    inputContraseña.blur()
-  })
+formLogin.addEventListener("submit", (e) => {
+  e.preventDefault();
+  validarDatos();
+  formLogin.reset();
+  inputContraseña.blur();
+});
 
-  function validarDatos() {
-    let usuarioIngresado = inputUsuario.value.trim()
-    let contraseñaIngresada = inputContraseña.value.trim()
-    let esTexto = /^[a-zA-Z]+$/;
-    if (esTexto.test(usuarioIngresado) && esTexto.test(contraseñaIngresada) && usuarioIngresado !== "" && contraseñaIngresada !== "") {
+function validarDatos() {
+  let usuarioIngresado = inputUsuario.value.trim();
+  let contraseñaIngresada = inputContraseña.value.trim();
+  let esTexto = /^[a-zA-Z]+$/;
+  if (
+    esTexto.test(usuarioIngresado) &&
+    esTexto.test(contraseñaIngresada) &&
+    usuarioIngresado !== "" &&
+    contraseñaIngresada !== ""
+  ) {
+    if (
+      usuarioIngresado === usuarioAdmin &&
+      contraseñaIngresada === contraseñaAdmin
+    ) {
       console.log("Administrador logueado.");
     } else {
-      console.log("Usuario sin acceso.");
+      if (
+        usuarioIngresado !== usuarioAdmin &&
+        contraseñaIngresada === contraseñaAdmin
+      ) {
+        console.log("El usuario ingresado es incorrecto.");
+      } else if (
+        usuarioIngresado === usuarioAdmin &&
+        contraseñaIngresada !== contraseñaAdmin
+      ) {
+        console.log("La contraseña ingresada es incorrecta.");
+      } else {
+      console.log("Usuario invalido.");
+      }
     }
+  } else {
+    console.log("Los datos ingresados son erroneos");
   }
+}
