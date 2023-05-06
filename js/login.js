d7348a7a051af8eabe8d150ddf27be14748208a2
Nicolas Herrera <herreranicoo17@gmail.com>
2023-05-06 14:06:05 -0300

    Declare variables globales iniciales en login.js

---
+++ js/login.js
@@ -0,0 +1,5 @@
+const usuarioAdmin = "administrador",
+  contraseñaAdmin = "administrador",
+  inputUsuario = document.getElementById("inputUsuario"),
+  inputContraseña = document.getElementById("inputContraseña"),
+  btnIniciarSesion = document.getElementById("btnIniciarSesion");
