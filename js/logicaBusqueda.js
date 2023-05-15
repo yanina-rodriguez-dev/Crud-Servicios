9b3cedadfc2ecd12333d247d28a563487f6a63eb
Yanina Alejandra Rodríguez <121127071+yanina-rodriguez-dev@users.noreply.github.com>
2023-05-15 13:10:32 -0300

    detener recarga e prevent y traer dato del usuario

--- js/logicaBusqueda.js
+++ js/logicaBusqueda.js
@@ -2,3 +2,10 @@ let campoDeBusqueda = document.getElementById("campoDeBusqueda");
 let resultadosDeBusqueda = document.getElementById("resultadosDeBusqueda");
 
 let formularioBusqueda = document.getElementById("formularioBusqueda");//traer
+  //detener recarga e prevent y traer dato del usuario
+formularioBusqueda.addEventListener("submit", (e) => {
+    e.preventDefault();
+    buscarArticulo();
+    formularioBusqueda.reset();
+  });
+  
