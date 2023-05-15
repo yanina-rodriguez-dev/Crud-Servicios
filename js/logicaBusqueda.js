0b2aef926106ccf1cb04e30aa2da51922154594f
Yanina Alejandra Rodríguez <121127071+yanina-rodriguez-dev@users.noreply.github.com>
2023-05-15 13:02:38 -0300

    creo variables principales y las traigo por id

---
+++ js/logicaBusqueda.js
@@ -0,0 +1,4 @@
+let campoDeBusqueda = document.getElementById("campoDeBusqueda");
+let resultadosDeBusqueda = document.getElementById("resultadosDeBusqueda");
+
+let formularioBusqueda = document.getElementById("formularioBusqueda");//traer
