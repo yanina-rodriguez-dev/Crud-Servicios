28c9a7a578c201bd03c9528ae44d7d6add063046
Yanina Alejandra Rodríguez <121127071+yanina-rodriguez-dev@users.noreply.github.com>
2023-05-15 23:08:24 -0300

    desaparecer lo que no coincida con la busqueda y mostrar mensaje de no se encontraron coincidencias, ademas cambio color del texto y elimino fondo porque se confundia con las letras

--- js/logicaBusqueda.js
+++ js/logicaBusqueda.js
@@ -8,4 +8,33 @@ formularioBusqueda.addEventListener("submit", (e) => {
     buscarArticulo();
     formularioBusqueda.reset();
   });
+  function buscarArticulo() {
+    let textoBuscado = campoDeBusqueda.value.trim();
+    textoBuscado = textoBuscado.toLowerCase();
+    let articulos = document.getElementsByClassName("articulo");
   
+    let articuloEncontrado;
+    
+      //desaparecer lo que no coincida
+  
+    for (let i = 0; i < articulos.length; i++) {
+      let articuloRecorrido = articulos[i].innerHTML.toLowerCase().includes(textoBuscado);
+      if (articuloRecorrido === true) {
+        articuloEncontrado = articulos[i];
+        
+        articuloEncontrado.style.display = "flex";
+      } else {
+        articulos[i].style.display = "none";
+  
+      }
+    } 
+    if (!articuloEncontrado){
+      let mensaje = document.getElementById("sincoincidencias");
+      mensaje.className = 'fs-1 text-center my-5';
+      
+    }
+    if (!articuloEncontrado){
+      
+    }
+   
+  }
