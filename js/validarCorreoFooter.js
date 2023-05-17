21261e7e4dd74abc8883677ffe29dacc90f62dcd
Flor <flor_cane@hotmail.com>
2023-05-17 21:04:06 +0200

    funcion validar

--- js/validarCorreoFooter.js
+++ js/validarCorreoFooter.js
@@ -2,3 +2,6 @@ let input_correo_suscribirse = document.getElementById("input_correo_suscribirse
 let boton_suscribirse = document.getElementById("botonsuscribirse");
 let form = document.querySelector("#formSuscripcion");
 const patronValidadCorreo = /^[a-zA-Z0-9.%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/;
+
+function validarCorreo(){
+    let valueInputCorreo = input_correo_suscribirse.value;
