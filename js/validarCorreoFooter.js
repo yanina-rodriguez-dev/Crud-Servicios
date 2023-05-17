7a492ff80579a97f00556d7889f07da0ef436bf3
Flor <flor_cane@hotmail.com>
2023-05-17 21:04:49 +0200

    info de error

--- js/validarCorreoFooter.js
+++ js/validarCorreoFooter.js
@@ -5,3 +5,14 @@ const patronValidadCorreo = /^[a-zA-Z0-9.%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/;
 
 function validarCorreo(){
     let valueInputCorreo = input_correo_suscribirse.value;
+    if(patronValidadCorreo.test(valueInputCorreo)){
+
+    }else{
+        /* Informar al usuario de que debe contener @ y . en su correo */
+        let informeDeError = document.createElement("p");
+        informeDeError.innerHTML = "El correo debe contener @ y .";
+        informeDeError.classList.add("fs-4");
+        informeDeError.classList.add("text-white");
+        form.appendChild(informeDeError);
+    }
+}
