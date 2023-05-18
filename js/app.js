a975087a373c073fa2c12f6df9cefd70f4df5f5f
Gerardo Mansilla <gerardoemansilla@gmail.com>
2023-05-18 00:22:17 -0300

    detalles minimos de diseño

--- js/app.js
+++ js/app.js
@@ -8,11 +8,11 @@ console.log(box);
 //nav.className = ('navbar navbar-expand-lg bg-transparent fixed-top');
 window.addEventListener("scroll", () => {
   if (window.scrollY >= box.offsetHeight) {
-    nav.classList.add("bg-blue");
+    nav.classList.add("bg-dark");
     nav.classList.remove("bg-transparent");
-    //nav.className = ('navbar navbar-expand-lg bg-blue fixed-top');
+    //nav.className = ('navbar navbar-expand-lg bg-dark fixed-top');
   } else if (window.scrollY < box.offsetHeight) {
-    nav.classList.remove("bg-blue");
+    nav.classList.remove("bg-dark");
     nav.classList.add("bg-transparent");
     //nav.className = ('navbar navbar-expand-lg bg-transparent fixed-top');
   }
--- js/detalle.js
+++ js/detalle.js
@@ -1,21 +1,8 @@
 const parametroURL = new URLSearchParams(window.location.search);
 
-console.log(parametroURL);
-console.log(parametroURL.get('codigo'));
-
-
 let listaPaquetes = JSON.parse(localStorage.getItem("listaPaquetes")) || [];
 
-
 const paqueteBuscado = listaPaquetes.find((paquete) => paquete.codigo === parametroURL.get('codigo'));
-
-/* const paquetePrueba = {
-    nombre: "Maldivas",
-    precio: "1.3M",
-    categoria: "Asia",
-    descripcion: "Maldivas es un país tropical en el océano Índico que abarca 26 atolones con forma de anillos que se componen de más de 1,000 islas de coral. Es conocido por las playas, las lagunas azules y los extensos arrecifes. La capital, Malé, tiene un mercado de pescado ajetreado, restaurantes y tiendas en la avenida principal, Majeedhee Magu, y la mezquita del siglo XVII Hukuru Miskiy (también conocida como la Mezquita del Viernes), hecha de coral blanco tallado.",
-    dias: "13"
-} */
 
 /* render detalle paquete */
 let labelNombre = document.getElementById('paquete-nombre');
@@ -30,4 +17,3 @@ labelNombre.innerHTML = paqueteBuscado.nombre;
 labelDescripcion.innerHTML = paqueteBuscado.descripcion;
 labelCodigo.innerHTML = paqueteBuscado.codigo;
 labelPrecio.innerHTML = `${paqueteBuscado.precio} $`;
-
